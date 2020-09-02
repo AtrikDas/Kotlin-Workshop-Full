da85cb19adaed045397538004e3a48400c84e7da
Atrik Das <atrikdas@gmail.com>
2020-09-02 17:48:41 +0800

    created empty questions activity and added flag images

---
+++ app/src/main/java/com/example/flag_match/QuizQuestionsActivity.kt
@@ -0,0 +1,11 @@
+package com.example.flag_match
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class QuizQuestionsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_quiz_questions)
+    }
+}
