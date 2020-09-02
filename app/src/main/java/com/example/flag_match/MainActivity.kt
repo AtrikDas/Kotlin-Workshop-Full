a89dd2c7f5b45c5c408f3bb3235d16c1ceeeb37a
Atrik Das <atrikdas@gmail.com>
2020-09-02 17:09:06 +0800

    create layout for main activity

--- app/src/main/java/com/example/flag_match/MainActivity.kt
+++ app/src/main/java/com/example/flag_match/MainActivity.kt
@@ -2,10 +2,14 @@ package com.example.flag_match
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
+
     }
 }
