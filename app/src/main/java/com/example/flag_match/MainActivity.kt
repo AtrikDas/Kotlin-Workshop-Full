81ed972a54d21807d32065d1d0c5fd46989167cf
Atrik Das <atrikdas@gmail.com>
2020-09-03 00:06:43 +0800

    Create questions

--- app/src/main/java/com/example/flag_match/MainActivity.kt
+++ app/src/main/java/com/example/flag_match/MainActivity.kt
@@ -1,13 +1,26 @@
 package com.example.flag_match
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btn_start.setOnClickListener{
+            if (et_name.text.toString().isEmpty()){
+                Toast.makeText(this, "Please enter your name", Toast.LENGTH_SHORT).show()
+            }else{
+                val intent = Intent(this, QuizQuestionsActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
+        }
 
     }
 }
--- app/src/main/java/com/example/flag_match/QuizQuestionsActivity.kt
+++ app/src/main/java/com/example/flag_match/QuizQuestionsActivity.kt
@@ -7,5 +7,7 @@ class QuizQuestionsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_quiz_questions)
+
+        val questionsList = Constants.getQuestions()
     }
 }
