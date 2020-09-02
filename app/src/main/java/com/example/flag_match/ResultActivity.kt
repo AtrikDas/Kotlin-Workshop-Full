02571958c8565e45a01a007d6b754769ee07c5df
Atrik Das <atrikdas@gmail.com>
2020-09-02 18:36:15 +0800

    created results layout

--- app/src/main/java/com/example/flag_match/ResultActivity.kt
+++ app/src/main/java/com/example/flag_match/ResultActivity.kt
@@ -2,12 +2,10 @@ package com.example.flag_match
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
 
-class MainActivity : AppCompatActivity() {
+class ResultActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
+        setContentView(R.layout.activity_result)
     }
 }
